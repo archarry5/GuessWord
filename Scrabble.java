b733973fd6d2d4857e805bfaf56dcd7c1a2d1847
Archana Kumari <Archana.Kumari@cdk.com>
2016-07-10 16:08:28 +0530

    added createWordList()

--- Scrabble.java
+++ Scrabble.java
@@ -1,10 +1,23 @@
 public class Scrabble {
 	String word;
 	int level;
-	
+	Arraylist<String> wordList;	
+
 	public Scrabble(int level) {
 		this.level = level;
+		wordList = new ArrayList<String>();
 	}
 	
-	
+	public void createWordList(int size) {
+		BufferedReader buffReader = null;
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("/home/administrator/GitRepo/GuessWord/sowpods"));
+			String word = "";
+			while ((word = reader.readLine()) != null) {
+				if (word.length() == level) {
+					wordList.add(word);
+				}			
+			} 
+		}
+	}
 }
