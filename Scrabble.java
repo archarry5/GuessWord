3f183976220d21241894f9e87f174489a9a4a4f1
Archana Kumari <Archana.Kumari@cdk.com>
2016-07-11 09:08:43 +0530

    modified updateList()

--- Scrabble.java
+++ Scrabble.java
@@ -35,25 +35,73 @@ public class Scrabble {
 		Random r = new Random();
 		int index = r.nextInt(wordList.size());
 		return wordList.get(index);
+	} 
+
+	public int evaluateGuess(String guess) {
+		int count = 0;
+		guess = removeDuplicate(guess);
+		for(int i = 0;i < guess.length(); i++) {
+			char c = guess.charAt(i);
+			
+			if (word.contains(guess.charAt(i) + "" )) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	public String removeDuplicate(String word){
+	    char[] processed = word.toCharArray();
+	    for(int i = 0; i < word.length(); i++){
+	    	for(int j = i+1 ; j < word.length(); j++) {
+	    		if(processed[i] == processed[j]) {
+	    			processed[j] = ' ';
+	    		}
+	    	}
+	    } 
+	    return new String(processed);
 	}
 
 	public void updateList(String guess, int count) {
+		guess = removeDuplicate(guess);
 		if (count == 0) {
 			//remove all words with chars in string guess
 			for (char c : guess.toCharArray()) {
-				ArrayList<String> copyOfList = ()wordList.clone();
-				for (String word : copyOfList) {
+				for (int i = 0; i < wordList.size(); i++) {
+					String word = wordList.get(i);					
 					if (word.contains(c + ""))
-						wordList.remove(word);
+						wordList.set(i,"");
 				}
-			}
+				wordList.removeAll(Arrays.asList(""));
+			} 
 			for (String word : wordList) 
 				System.out.println(word);
 		} else if (count == wordSize) {
 			// remove all words who donot hv even 1 of the above chars
+			char[] ascendingGuess = guess.toCharArray();
+			Arrays.sort(ascendingGuess);
+			for (int i = 0; i < wordList.size(); i++) {
+				char[] wordArr = word.toCharArray();
+				Arrays.sort(wordArr);			
+				if (!Arrays.equals(wordArr, ascendingGuess))
+					wordList.set(i,"");
+			}
+			wordList.removeAll(Arrays.asList(""));
 		}
 		else {
 			//remove all words who donot hv any of these characters
+			int[] countMissedLett = new int[wordList.size()];
+			for (int i = 0; i < wordList.size(); i++) {
+				String word = wordList.get(i);					
+				for (char c : guess.toCharArray()){									
+					if (!word.contains(c + "")) {
+						countMissedLett[i]++;				
+					}
+				}
+				if (countMissedLett[i] == wordSize)						
+					wordList.set(i,"");	
+			}
+			wordList.removeAll(Arrays.asList(""));
 		}
 	}
 
