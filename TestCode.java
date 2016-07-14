d98c410b5af49bb96bf3a12351d1c4e71e98fd2e
Archana Kumari <Archana.Kumari@cdk.com>
2016-07-14 12:24:15 +0530

    added test code

--- TestCode.java
+++ TestCode.java
@@ -1,8 +1,30 @@
-
+import java.util.*;
 public class TestCode {
 	public static void main(String args[]) {
-		Scrabble MindGame = new Scrabble(4);
-		String DecidedWord = args[0];
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter word size u want to play..");
+		int wordSize = sc.nextInt(); 
+		Scrabble scrabble = new Scrabble(wordSize);
+		scrabble.chooseWord();
+		while (!scrabble.gameWon) {
+			String compGuess = scrabble.guessWord();			
+			System.out.println(compGuess + "\n Enter no. of char matched -1 if word is guessed correct");
+			int compResult = sc.nextInt();
+			if (compResult == -1) {
+				System.out.println("Computer Won");
+				break;
+			}
+			scrabble.updateList(compGuess, compResult);
+			System.out.println("Your turn to guess..");
+			String userGuess = sc.next();
+			int userResult = scrabble.evaluateGuess(userGuess);
+			if (userResult == -1)
+				System.out.println("You Won");
+			else
+				System.out.println(userResult+ " character matched");
+			
+		}
+/*
 		int result = 0;
 		String Guess = " ";
 		while (result != 5) {
@@ -15,7 +37,6 @@ public class TestCode {
 		}
 		System.out.println(result);
 		System.out.println(MindGame.guessWord());
-		System.out.println("Won the game");
+		System.out.println("Won the game"); */
 	}
 }
-
